aad688dc67f0402dfe2a99b504d8e430c871aca2
HoangTechCS-Cyber <nguyenhuyhoangqbx5@gmail.com>
2024-10-14 18:50:51 +0700

    Update hoocne.cpp

--- hoocne.cpp
+++ hoocne.cpp
@@ -1,36 +1,44 @@
-#include <bits/stdc++.>
-#define int long long
+#include <bits/stdc++.h>
+
 using namespace std;
 
-float hoocne(int n, float c, float a[]) {
-    float p = a[0]; 
-    for (int i = 1; i <= n; i++) {
-        p = p * c + a[i]; 
-    }
-    return p;
+#define ld long double
+#define For(i, a, b) for (int i = a; i <= b; i++)
+#define pii pair<int, int>
+
+const int MOD = 1e9 + 7;
+const int N = 108;
+const int maxn = 150000;
+int i, n;
+float c, p, a[10];
+
+
+void GetInput()
+{
+    cout << "Nhap gia tri can tinh: "; cin >> c;
+    cout << "Nhap bac cua da thuc: ";  cin >> n;
+    cout << "Nhap cac he so: ";
+    For(i,0,n) {cout << "a["<<i<<"] = "; cin >> a[i];}
+    p = a[0];
+
 }
 
-signed main() {
-    int n;
-    float c;
+void solve()
+{
+    For(i,1,n) p = p*c + a[i];
 
-    cout << "Nhap bac da thuc: ";
-    cin >> n;
-
-    float a[n+1]; 
-
-    cout << "Nhap cac he so cua da thuc:\n";
-    for (int i = 0; i <= n; i++) {
-        cout << "a[" << i << "] = ";
-        cin >> a[i];
-    }
-
-    cout << "Nhap gia tri can tinh (c): ";
-    cin >> c;
-
-    float result = hoocne(n, c, a);
-    cout << "Gia tri da thuc tai c = " << c << " la: " << result << endl;
-
-    return 0;
 }
-// node
+void rep()
+{
+    cout << "Gia tri cua da thuc la: " << p << endl;
+}
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    GetInput();
+    solve();
+    rep();
+}
